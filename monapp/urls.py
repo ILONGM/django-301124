6f6718f2efedb4236a97c93c8dd4b462eeafc205
mathieu <barletmathieu@gmail.com>
2024-12-20 22:08:00 +0100

    creation d'une vue portfolio et détail d'un portfolio

--- monapp/urls.py
+++ monapp/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path("",views.home, name="home"),
-    path("todo/",views.todolist,name="bdd")
+    path("todo/",views.todolist,name="bdd"),
+
+    #create Urls for views displying portfolios & holdings
+    path('portfolios/', views.list_portfolios, name='list_portfolios'),
+    path('portfolios/<int:portfolio_id>/', views.view_portfolio, name='view_portfolio'),
 ]
--- monapp/views.py
+++ monapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
-from .models import TodoItem
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import TodoItem, Portfolio
 
 # a views function takes a request and return a response
 # views is a request handler
@@ -12,3 +12,13 @@ def todolist (request):
     todos = TodoItem.objects.all()
     return render(request, 'TodoItem.html', {'todos': todos})
 # Create your views here.
+
+# create a view that list portfolios
+def list_portfolios(request):
+    portfolios = Portfolio.objects.all()
+    return render(request, 'list.html', {'portfolios': portfolios})
+
+def view_portfolio(request, portfolio_id):
+    portfolio = get_object_or_404(Portfolio, id=portfolio_id)
+    holdings = portfolio.holdings()
+    return render(request, 'detail.html', {'portfolio': portfolio, 'holdings': holdings})
